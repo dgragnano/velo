3ea3535d11465707497f946ac3ae6a5af9e6929e
Douglas Gragnano <douglas.gragnano@gmail.com>
2026-02-01 17:24:53 -0300

    novas alterações de código

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -1,8 +1,12 @@
 import { test, expect } from '@playwright/test';
 
+/// AAA - Arrange, Act, Assert
+
 test('deve consultar um pedido aprovado', async ({ page }) => {
   await page.goto('http://localhost:5173/');
 
+
+  // Arrange
   //Checkpoint  
   await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint');
 
@@ -11,11 +15,13 @@ test('deve consultar um pedido aprovado', async ({ page }) => {
   //Checkpoint
   await expect(page.getByRole('heading')).toContainText('Consultar Pedido');
 
-  //await page.getByTestId('search-order-id').click();
 
-  await page.getByTestId('search-order-id').fill('VLO-MMXKY1');
+  // Act
+  await page.getByRole('textbox', { name: 'Número do Pedido' }).fill('VLO-MMXKY1');
 
   await page.getByTestId('search-order-button').click();
+
+  // Assert
 
   await expect(page.getByTestId('order-result-id')).toContainText('VLO-MMXKY1');
 
